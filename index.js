86625aeca8afba787482eaea066c155604d49340
Madison Vega <madison.r.vega@gmail.com>
2021-03-30 21:36:30 -0500

    added module exports to each class and added appropriate requirements to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// imports
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
+
+
